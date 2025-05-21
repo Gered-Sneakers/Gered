a334a0f177791b821a75f9e53afa324df6b89f41
Gered <sale@gered.be>
2025-05-21 11:23:41 +0200

    Revert server files to 15 mei state

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const uploadRoutes = require('./src/routes/image.routes');
 const app = express(); 
  
 var corsOptions = { 
-  origin: 'https://g-app.be/api' 
+  origin: "http://localhost:8081" 
 }; 
 
 // cors
@@ -23,17 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/brands', express.static(path.join(__dirname, 'public/uploads')));
 app.use('/api', uploadRoutes);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve frontend build
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Route fallback
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 const db = require("./src/models"); 
 db.sequelize.sync();
 require("./src/routes/sneaker.routes.js")(app);
@@ -42,11 +31,10 @@ require("./src/routes/werknemer.routes.js")(app);
 require("./src/routes/brand.routes.js")(app); 
 
 // simple route 
-/*
 app.get("/", (req, res) => { 
   res.json({ message: "Welcome to bezkoder application." }); 
 }); 
-*/
+
 app._router.stack
   .filter(r => r.route)
   .forEach(r => console.log("📦 Route:", r.route.path));
--- src/http-common.js
+++ src/http-common.js
@@ -1,11 +1,11 @@
 import axios from "axios"; 
 
 const baseURL = import.meta.env.PROD
-  ? 'https://g-app.be/api'
-  : 'https://g-app.be'
-  //: 'http://localhost:8080/api';
+  ? 'https://g-app.be'
+  : 'http://localhost:8080/api';
  
 export default axios.create({ 
-  baseURL: baseURL, 
+  baseURL: "http://localhost:8080/api", 
+  //baseURL,
   headers: { "Content-type": "application/json" } 
 }); 
