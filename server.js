dd07821c507842d3c523dc26c02d3edf443f5aab
Gered <sale@gered.be>
2025-09-10 12:45:55 +0200

    double check put online

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ const ALLOWED_ORIGINS = [
   "http://127.0.0.1:5173",
   "http://localhost:8081",
   "http://127.0.0.1:8081",
+  "http://localhost:5000",
+  "http://127.0.0.1:5000",
 ].filter(Boolean);
 
 app.use(
